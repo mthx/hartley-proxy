1b8ed7f9aafd7f1398d60d11ba8aad82c3353efd
Matt Hillsdon <mth@corefiling.com>
2017-11-04 23:20:50 +0000

    Basic http-only proxy works.
    Needs careful testing and review.
    Not tried outside of the artificial test environment.

--- __tests__/proxy-spec.ts
+++ __tests__/proxy-spec.ts
@@ -1,11 +1,46 @@
+import * as http from 'http';
 import * as fetch from 'node-fetch';
+import * as fs from 'fs';
+
+import { BackendServer } from '../src/backend-server';
 import { Proxy } from '../src/proxy';
 
+interface IRequestResult {
+  response: http.IncomingMessage;
+  body: string;
+}
+
+async function proxyRequest(proxy: Proxy, backend: BackendServer): Promise<IRequestResult> {
+  return new Promise<IRequestResult>((resolve, reject) => {
+    const options: http.RequestOptions = {
+      headers: {
+        host: backend.hostAndPort(),
+      },
+      hostname: 'localhost',
+      path: backend.url() + '/fish',
+      port: proxy.port(),
+    };
+    let body = '';
+    const request = http.request(options, (response: http.IncomingMessage) => {
+      response.on('data', chunk => body += chunk.toString());
+      response.on('end', () => resolve({response, body}));
+    });
+    request.on('error', e => reject(e));
+    request.end();
+  });
+}
+
 describe('proxy', () => {
-  it('starts and stops', async () => {
+  it('proxies a simple request', async () => {
+    const backend = new BackendServer();
     const proxy = new Proxy({});
-    await proxy.listen();
-    await fetch(proxy.url())
-    await proxy.close();
+
+    await Promise.all([proxy.listen(), backend.listen()]);
+
+    const {response, body} = await proxyRequest(proxy, backend);
+    expect(response.statusCode).toEqual(200);
+    expect(body).toEqual('Hello, World!');
+
+    await Promise.all([proxy.close(), backend.close()]);
   });
 });

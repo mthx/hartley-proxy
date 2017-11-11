e6848aca2fd32147fbd7369277dc6a54cff54680
Matt Hillsdon <matt@hillsdon.net>
2017-11-11 19:30:26 +0000

    Tests.

--- __tests__/writables-spec.ts
+++ __tests__/writables-spec.ts
@@ -2,12 +2,32 @@ import {PassThrough} from 'stream';
 import {BufferWritable} from '../src/writables';
 
 describe('BufferWritable', () => {
-  it('copies to buffer', () => {
-    const dest = new BufferWritable();
-    const source = new PassThrough();
+
+  let dest: BufferWritable;
+  let source: PassThrough;
+
+  beforeEach(() => {
+    dest = new BufferWritable();
+    source = new PassThrough();
     source.pipe(dest);
+  });
+
+  it('copies to buffer', () => {
     source.write('foo');
     source.end('bar');
     expect(dest.toBuffer().toString()).toEqual('foobar');
+    expect(dest.toBuffer()).toEqual(dest.toBuffer());
+    expect(dest.toBuffer()).not.toBe(dest.toBuffer());
   })
+
+  it('returns a new copy empty', () => {
+    expect(dest.toBuffer()).not.toBe(dest.toBuffer());
+  })
+
+  it('returns a new copy after single write', () => {
+    // Special case in the impl.
+    source.end('foo');
+    expect(dest.toBuffer()).not.toBe(dest.toBuffer());
+  })
+
 })
